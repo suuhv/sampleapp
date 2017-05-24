a83254a9e148d32974161f7aa02a904b160c655c
Suu ha <havansuu29111995@gmail.com>
2017-05-24 15:01:33 +0700

    chap 9: login:

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,10 @@ class UsersController < ApplicationController
   def show
   	@user = User.find(params[:id])	
   end
+  def index
+    @users = User.paginate(page: params[:page])
+    
+  end
 
   def new
   	@user = User.new
