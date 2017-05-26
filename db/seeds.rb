54a7666de61fc68f774b59a02aa9ea1034174da7
Suu ha <havansuu29111995@gmail.com>
2017-05-26 13:11:46 +0700

    chap 10: update, show, delete

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,10 @@
-User.create!(name:  "Example User",
-             email: "example@railstutorial.org",
-             password:              "foobar",
-             password_confirmation: "foobar")
+User.create!(name:  "Suu",
+             email: "suu@123.com",
+             password:              "123123",
+             password_confirmation: "123123",
+             admin: true)
 
-999.times do |n|
+99.times do |n|
   name  = Faker::Name.name
   email = "example-#{n+1}@railstutorial.org"
   password = "password"
